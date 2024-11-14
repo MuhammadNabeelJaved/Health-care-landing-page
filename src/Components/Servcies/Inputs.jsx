67176e234fff90f9c00311148f6a73bfdd09ec03
Muhammad Nabeel Javed <muhammadnabeeljavede@gmail.com>
2024-11-15 00:16:21 +0500

    Make an input component. also making services section and correcting services files class and element name servcies to services

---
+++ src/Components/Servcies/Inputs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./servcies.css";
+
+const Inputs = ({ type, placeholder, icon }) => {
+  return (
+    <>
+      <div>
+        <img className="input-search-icon" src={icon} alt="" />
+        <input
+          className="input"
+          type={type}
+          name=""
+          id=""
+          placeholder={placeholder}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Inputs;
